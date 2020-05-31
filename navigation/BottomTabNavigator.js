04db9efae4d296aadfa0528193293695f7f3d825
yujiez <yujiez@sfu.ca>
2020-05-30 17:19:22 -0700

    set base structure

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -3,10 +3,11 @@ import * as React from 'react';
 
 import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
-import LinksScreen from '../screens/LinksScreen';
+import ItemScreen from '../screens/ItemsScreen';
+import ToBuyScreen from '../screens/ToBuyScreen';
 
 const BottomTab = createBottomTabNavigator();
-const INITIAL_ROUTE_NAME = 'Home';
+const INITIAL_ROUTE_NAME = 'Overview';
 
 export default function BottomTabNavigator({ navigation, route }) {
   // Set the header title on the parent stack navigator depending on the
@@ -17,19 +18,27 @@ export default function BottomTabNavigator({ navigation, route }) {
   return (
     <BottomTab.Navigator initialRouteName={INITIAL_ROUTE_NAME}>
       <BottomTab.Screen
-        name="Home"
+        name="overview"
         component={HomeScreen}
         options={{
-          title: 'Get Started',
+          title: 'Overview',
           tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-code-working" />,
         }}
       />
       <BottomTab.Screen
-        name="Links"
-        component={LinksScreen}
+        name="items"
+        component={ItemScreen}
         options={{
-          title: 'Resources',
+          title: 'Items',
           tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-book" />,
+        }}
+      />
+      <BottomTab.Screen
+        name="tobuy"
+        component={ToBuyScreen}
+        options={{
+          title: 'To Buy',
+          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-barcode" />,
         }}
       />
     </BottomTab.Navigator>
@@ -40,9 +49,11 @@ function getHeaderTitle(route) {
   const routeName = route.state?.routes[route.state.index]?.name ?? INITIAL_ROUTE_NAME;
 
   switch (routeName) {
-    case 'Home':
+    case 'Overview':
       return 'How to get started';
-    case 'Links':
+    case 'Items':
+      return 'Links to learn more';
+    case 'To Buy':
       return 'Links to learn more';
   }
 }
